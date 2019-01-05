d13effde33a1e440e59edd627806452395f546e1
Brent Beer <brent.beer@gmail.com>
2019-01-05 16:52:00 +0100

    Initial Commit

---
+++ KindleReadingTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KindleReadingTime
+//
+//  Created by Brent Beer on 1/5/19.
+//  Copyright © 2019 brntbeer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ KindleReadingTimeTests/KindleReadingTimeTests.swift
@@ -0,0 +1,34 @@
+//
+//  KindleReadingTimeTests.swift
+//  KindleReadingTimeTests
+//
+//  Created by Brent Beer on 1/5/19.
+//  Copyright © 2019 brntbeer. All rights reserved.
+//
+
+import XCTest
+@testable import KindleReadingTime
+
+class KindleReadingTimeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
