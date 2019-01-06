70d1f0be37d1e0eb5a1621fa698661997339209b
Brent Beer <brent.beer@gmail.com>
2019-01-06 18:50:05 +0100

    add second book and print out all book pages in order

--- KindleReadingTime/ViewController.swift
+++ KindleReadingTime/ViewController.swift
@@ -37,8 +37,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = .red
 
-        let page1 = Page(number: 1, text: "Text for first page1")
-        let page2 = Page(number: 2, text: "Text for page 2")
+        let page1 = Page(number: 1, text: "Text for book1 page 1")
+        let page2 = Page(number: 2, text: "Text for book1 page 2")
 
         let pages = [page1, page2]
 
@@ -53,6 +53,21 @@ class ViewController: UIViewController {
         for page in book.pages {
             if page.number == 1 {
               print(page.text)
+            }
+        }
+
+        let book2 = Book(title: "Bill Gates: A Biography", author: "Michael Becraft", pages: [
+            Page(number: 1, text: "Text in book2 for page 1"),
+            Page(number: 2, text: "Text in book2 for page 2"),
+            Page(number: 3, text: "Text in book2 for page 3"),
+            Page(number: 4, text: "Text in book2 for page 4"),
+
+            ])
+        let books = [book, book2]
+
+        for book in books {
+            for page in book.pages {
+                print(page.text)
             }
         }
     }
