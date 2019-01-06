35ced2a7dbf097754a14a97aaccd2e8568ad8953
Brent Beer <brent.beer@gmail.com>
2019-01-06 18:19:29 +0100

    remove some print statements

--- KindleReadingTime/ViewController.swift
+++ KindleReadingTime/ViewController.swift
@@ -40,18 +40,11 @@ class ViewController: UIViewController {
         let page1 = Page(number: 1, text: "Text for first page1")
         let page2 = Page(number: 2, text: "Text for page 2")
 
-        print(page1.text)
-        print(page2.text)
-
         let pages = [page1, page2]
 
         let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: pages)
-        //print(book.title, book.author)
-        print("Title: \(book.title) and Author: \(book.author)")
 
         let firstpage = book.pages[0]
-
-        print(firstpage.text)
     }
 
 }
