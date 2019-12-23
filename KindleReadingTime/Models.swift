a0ff2f8b45fdf3df8d1146b534973e2210d466e9
Brent Beer <brent.beer@gmail.com>
2019-12-23 14:59:47 +0100

    images need to be a part of the book class, and import all of UIKit now

--- KindleReadingTime/Models.swift
+++ KindleReadingTime/Models.swift
@@ -6,16 +6,18 @@
 //  Copyright © 2019 brntbeer. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class Book {
   let title: String
   let author: String
+  let image: UIImage
   let pages: [Page]
 
-  init(title: String, author: String, pages: [Page]){
+  init(title: String, author: String, image: UIImage, pages: [Page]){
     self.title = title
     self.author = author
+    self.image = image
     self.pages = pages
   }
 }
