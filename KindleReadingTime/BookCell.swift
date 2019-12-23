061aa32cd232c7a9556af25ec73cf92299740dde
Brent Beer <brent.beer@gmail.com>
2019-12-23 15:05:04 +0100

    initial BookCell code

---
+++ KindleReadingTime/BookCell.swift
@@ -0,0 +1,22 @@
+//
+//  BookCell.swift
+//  KindleReadingTime
+//
+//  Created by Brent Beer on 12/23/19.
+//  Copyright © 2019 brntbeer. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+
+  //almost auto-generated override and required init? methods just so our cell is able to render itself
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+}
