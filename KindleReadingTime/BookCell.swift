00a6e26e056bc48a3862fe47b641331f69b928f4
Brent Beer <brent.beer@gmail.com>
2019-12-29 15:40:20 +0100

    add subviews for image and book title

--- KindleReadingTime/BookCell.swift
+++ KindleReadingTime/BookCell.swift
@@ -12,11 +12,29 @@ import UIKit
 // more freedom to define how we want the cell to look.
 class BookCell: UITableViewCell {
 
+  let coverImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.backgroundColor = .red
+    return imageView
+  }()
+
+  let titleLabel: UIImageView = {
+    let label = UILabel()
+    label.text = "This is the text from the the book for the title in our cell"
+    return label
+  }()
+
   //almost auto-generated override and required init? methods just so our cell is able to render itself
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
 
     backgroundColor = .yellow
+
+    addSubview(coverImageView)
+    coverImageView.frame = CGRect(x: 8, y: 8, width: 50, height: 64 )
+
+    addSubview(titleLabel)
+    titleLabel.frame = CGRect(x: 66, y: 20, width: 200, height: 20)
   }
 
   required init?(coder: NSCoder) {
