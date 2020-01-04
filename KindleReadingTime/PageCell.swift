433987826e9e70520c80ebd1c64cdf4bf620c7d2
Brent Beer <brent.beer@gmail.com>
2020-01-04 14:03:12 +0100

    create subview, and then render it appropriately
    it's filling the whole pagecell

--- KindleReadingTime/PageCell.swift
+++ KindleReadingTime/PageCell.swift
@@ -9,10 +9,28 @@
 import UIKit
 class PageCell: UICollectionViewCell {
 
+  //create subview of UILabel
+  let textLabel: UILabel = {
+    let label = UILabel()
+    label.text = "SOME TEXT FOR OUR LABEL"
+    label.backgroundColor = .green
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
   override init(frame:CGRect) {
     super.init(frame: frame)
 
     backgroundColor = .yellow
+
+    addSubview(textLabel)
+    //textLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+    textLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+    textLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+    textLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    textLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+
+
   }
 
   required init?(coder: NSCoder) {
