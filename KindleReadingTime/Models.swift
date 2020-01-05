10e809f8a8e2c85938f25fbfff64b227db4e2e23
Brent Beer <brent.beer@gmail.com>
2020-01-05 14:01:38 +0100

    handle the pages array in the dictionary constructor

--- KindleReadingTime/Models.swift
+++ KindleReadingTime/Models.swift
@@ -24,7 +24,18 @@ class Book {
     self.title = dictionary["title"] as? String ?? ""
     self.author = dictionary["author"] as? String ?? ""
     self.image = #imageLiteral(resourceName: "steve_jobs")
-    pages = []
+    var bookPages = [Page]()
+
+    if let pagesDictionaries = dictionary["pages"] as? [[String: Any]] {
+      for pageDictionary in pagesDictionaries {
+        if let pageText = pageDictionary["text"] as? String {
+          let page = Page(number: 1, text: pageText)
+          bookPages.append(page)
+        }
+      }
+    }
+
+    pages = bookPages
   }
 }
 
