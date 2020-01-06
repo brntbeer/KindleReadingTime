fff8382bd80d2b1addb20e0e7c0be0343814dccf
Brent Beer <brent.beer@gmail.com>
2020-01-06 21:49:34 +0100

    grab the coverImgUrl from json and store it in each book

--- KindleReadingTime/Models.swift
+++ KindleReadingTime/Models.swift
@@ -13,17 +13,22 @@ class Book {
   let author: String
   let image: UIImage
   let pages: [Page]
+  let coverImageUrl: String
 
   init(title: String, author: String, image: UIImage, pages: [Page]){
     self.title = title
     self.author = author
     self.image = image
     self.pages = pages
+    self.coverImageUrl = ""
   }
   init(dictionary: [String: Any]) {
     self.title = dictionary["title"] as? String ?? ""
     self.author = dictionary["author"] as? String ?? ""
+    self.coverImageUrl = dictionary["coverImageUrl"] as? String ?? ""
+
     self.image = #imageLiteral(resourceName: "steve_jobs")
+
     var bookPages = [Page]()
 
     if let pagesDictionaries = dictionary["pages"] as? [[String: Any]] {
